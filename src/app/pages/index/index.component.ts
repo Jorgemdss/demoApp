57dc8101cfe3e33044e8c7548a64ebce85c58e4d
Jorge Santos <jmdsantos4@gmail.com>
2025-03-16 18:32:14 +0000

    tests

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -14,9 +14,25 @@ export class IndexComponent {
   ) { }
 
   public makeCall() {
-    this.httpClient.get<User[]>('http://localhost:5069/api/User').subscribe(d => {
-      this.usersData = d;
+    // var headers = new HttpHeaders();
+    // headers.append('Content-Type', 'application/json; charset=utf-8');
+    // headers.append('Accept', 'application/json');
+    // headers.append('Authorization', 'Bearer CfDJ8NTef07WxrNGg5-c21xUcyzGboIwJedHTCnTCfLgfB4Pq9JBg0cb-qSXAOKajD5DKnSVMiACLHBZtN7LG2reWkyLY9HoILP_YoPObDOjle_E2K_UIW30_qCAsTuhGRCzmQV6ixiDTyQGZT9ikdz-4gT6ZQD-OQaEv_3gBIUxs_8IjU98WSzTRwVPeF9ClmFbOycF8y9YEd6oIu4oUZESk0-cKb6SiS2KfW_Rqvu5MULE8w49EvUU660pJP1XbZxmwLpyCE9GjMlkxCXJnschVFnbZp09MNwduAf-5I965X_JZM5EhU7swxYLD2I_q1rx8RHu_CsadLy2XbNYs1sQY2e1hkD-MNCRkgEwQBz3mxDvilx_M0qp8-ftM0aZZpc3ac-YZSL5iLtvboHoZQ8rZg1sgI0YWfSPQaffxNTaM9jHZBrSvkP9GrcM5H1TmCZD1JHX8C7waqeYyw3_LjWiYQHacUUKHUGX-u_eJgdNtePWET58FlpYrlxZrhHfRVni0yJzPcpI9WsV4uWoSHK4JE7Hb1tnw6QZ3BHecognHHLwEhG4LzLtHrFYYxsHgPRdjvkCzXCjmWYMwOU34ODR0Ja3z2_PQf4Wl8N0zGPYrErb_nTwDdlCfDe4FKpVWlTfYCDRVLVLNmrAImsFypXqAPtH3UWyRrP-M4SUhJkdnJZDUwfcj3yjv68GMb_tVEOGUQ');
+    // console.log(headers);
+
+
+    const headers2 = new HttpHeaders({
+      'Content-Type': 'application/json; charset=utf-8',
+      'Accept': 'application/json',
+      'Authorization': 'Bearer CfDJ8NTef07WxrNGg5-c21xUcyzGboIwJedHTCnTCfLgfB4Pq9JBg0cb-qSXAOKajD5DKnSVMiACLHBZtN7LG2reWkyLY9HoILP_YoPObDOjle_E2K_UIW30_qCAsTuhGRCzmQV6ixiDTyQGZT9ikdz-4gT6ZQD-OQaEv_3gBIUxs_8IjU98WSzTRwVPeF9ClmFbOycF8y9YEd6oIu4oUZESk0-cKb6SiS2KfW_Rqvu5MULE8w49EvUU660pJP1XbZxmwLpyCE9GjMlkxCXJnschVFnbZp09MNwduAf-5I965X_JZM5EhU7swxYLD2I_q1rx8RHu_CsadLy2XbNYs1sQY2e1hkD-MNCRkgEwQBz3mxDvilx_M0qp8-ftM0aZZpc3ac-YZSL5iLtvboHoZQ8rZg1sgI0YWfSPQaffxNTaM9jHZBrSvkP9GrcM5H1TmCZD1JHX8C7waqeYyw3_LjWiYQHacUUKHUGX-u_eJgdNtePWET58FlpYrlxZrhHfRVni0yJzPcpI9WsV4uWoSHK4JE7Hb1tnw6QZ3BHecognHHLwEhG4LzLtHrFYYxsHgPRdjvkCzXCjmWYMwOU34ODR0Ja3z2_PQf4Wl8N0zGPYrErb_nTwDdlCfDe4FKpVWlTfYCDRVLVLNmrAImsFypXqAPtH3UWyRrP-M4SUhJkdnJZDUwfcj3yjv68GMb_tVEOGUQ'
     });
+    console.log(headers2);
+
+
+    this.httpClient.get<User[]>('http://localhost:5069/api/User', { headers: headers2 })
+      .subscribe(d => {
+        this.usersData = d;
+      });
   }
 }
 
